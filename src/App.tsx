ba876713f5e8f6a783025e06fdceb193062d11e7
potatolover68 <thewonderfulworldofpotatoes@gmail.com>
2024-12-04 20:50:38 -0800

    chuck norrisifier

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      width: '100vw',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <div style={{
+        textAlign: 'center'
+      }}>
+        🎉dunce.party coming soon🎉
+        <br/>
+        have a chuck norris fact while you wait: <br /> <br />
+        <span id = 'chuck'>chuck norris is so strong that he broke into your house to turn back on the wifi</span>
+      </div>
     </div>
   );
 }
